4daf8892bf3e9e05ef9f7a68e3b810eba20c2aac
Nicolas Vargas <ndvargas95@gmail.com>
2024-04-23 23:30:59 -0500

    Functional version (not tested) 0.9.0

---
+++ src/bot.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# pylint: disable=unused-argument
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+TODO
+"""
+
+import logging
+from datetime import datetime, timedelta
+from os import environ, path as os_path
+from dotenv import load_dotenv
+
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
+
+import Scraper
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+# Dotenv load
+dotenv_path = os_path.join(os_path.dirname(__file__), '../.env')
+load_dotenv(dotenv_path)
+
+CITY, CATEGORY, RESULT = range(3)
+
+CM = Scraper.load_categories_map()
+
+rep_cities = Scraper.get_cities()
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """TODO"""
+
+    cities = [[c] for c in rep_cities.keys()]
+    reply_keyboard = cities
+    await update.message.reply_text(
+        "¡Hola! Soy pypcol_bot. Te puedo brindar información del pico y placa hoy en Colombia.\n\n"
+        "Elige una ciudad:"
+        ,
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard, one_time_keyboard=True, input_field_placeholder="¿Ciudad?"
+        ),
+    )
+
+    return CITY
+
+
+async def city(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """TODO"""
+    city = update.message.text
+    context.user_data['city'] = rep_cities[city]
+    logger.info("Chosen city %s", city)
+    categories_in_use_url = Scraper.url_builder(Scraper.PYPHOY_URL, rep_cities[city], '', '')
+    logger.info(Scraper.get_categories_in_use(CM, categories_in_use_url))
+    reply_keyboard = [[CM[cat]['text']] for cat in CM if cat in Scraper.get_categories_in_use(CM, categories_in_use_url)]
+    
+    await update.message.reply_text(
+        "Elige una categoria de pico y placa: ",
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard, one_time_keyboard=True, input_field_placeholder="¿Categoria?"
+        ),
+    )
+
+    return CATEGORY
+
+
+async def category(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """TODO"""
+    choose_v_id_cm = {y['text']: x for x, y in CM.items()}
+    category = update.message.text
+    date = datetime.now() - timedelta(hours=5)
+    context.user_data['category'] = choose_v_id_cm[category]
+    context.user_data['date'] = date.date().__str__()
+    data = context.user_data
+    result = Scraper.get_pyp_info(Scraper.url_builder(Scraper.PYPHOY_URL, data['city'], CM[data['category']]['path'], ""))
+    data['info'] = result
+    logger.info(data)
+    
+    await update.message.reply_photo(CM[data['category']]['image_url'],
+        parse_mode='HTML',
+        caption="{} {} en {}\n".format(CM[data['category']]['emoji'],CM[data['category']]['text'], data['city'][1:].capitalize().replace('-', ' ')) +
+        "🚫 <b>Retricción: {}</b>\n".format(data['date']) +
+        "        Tienen restricción placas terminadas en: {}\n".format(data['info']['plate_num']) +
+        "        <b>Horario establecido: </b>\n" +
+        "{}".format("\n".join(["        * " + v for v in data['info']['banned_times']])),
+        reply_markup=ReplyKeyboardRemove()
+    )
+    del choose_v_id_cm
+
+    return ConversationHandler.END
+
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """TODO"""
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    await update.message.reply_text(
+        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ConversationHandler.END
+
+
+def main() -> None:
+    """Run the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token(environ.get("TG_BOT_TOKEN")).build()
+
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            CITY: [MessageHandler(filters.Regex("^({city_options})$".format(city_options='|'.join(list(rep_cities.keys())))), city)],
+            CATEGORY: [MessageHandler(filters.Regex("^({category_options})$".format(category_options='|'.join([CM[cat]['text'] for cat in CM]))), category)]
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
+    )
+
+    application.add_handler(conv_handler)
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/scraper.py
@@ -0,0 +1,93 @@
+import logging, sys, os, json, re
+import requests, dotenv
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+PYPHOY_URL = 'https://www.pyphoy.com'
+
+# Dotenv load
+dotenv_path = os.path.join(os.path.dirname(__file__), '../.env')
+dotenv.load_dotenv(dotenv_path)
+
+# Select source of categories
+
+def load_categories_map():
+    '''
+    TODO
+    '''
+    categories_file = (os.path.join(os.path.dirname(__file__), 'assets/categories.json') if os.environ.get("PYPHOY_TH") == 'dev' else 'PROD_STRING')
+    with open(categories_file, "r") as cat_file:
+        categories_map = json.load(cat_file)
+        cat_file.close()
+
+    return categories_map
+
+def get_cities() -> dict:
+    '''
+    TODO
+    '''
+    home_page = request_pyp_page(PYPHOY_URL)
+    _cities_matrix = [[t.text, t.attrs['value']] for t in home_page.find_all('option') if t.attrs.get('value')]
+    del home_page
+    return {k: v for k, v in _cities_matrix if not bool(re.search(k,"^(Armenia|Cúcuta)$")) }
+
+def get_categories_in_use(cg_map, city_url):
+    '''
+    TODO
+    '''
+    city_page = request_pyp_page(city_url)
+    _cities_text = ['/' + c.find('a').get('href').split("/", 2)[-1] for c in city_page.find_all("h2", {"class": "sc-d159082d-0"})]
+
+    return [c for c in cg_map if cg_map[c]['path'] in _cities_text]
+    
+
+def get_pyp_info(url):
+    '''
+    TODO
+    '''
+    options = Options()
+    options.add_argument('--headless=new') 
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver.get(url)
+    plate_nums_info = driver.find_element(By.XPATH, '//*[@id="__next"]/div/div[3]/main/article/div[1]/div[1]/div/div/div[2]').text
+    
+    if not driver.find_elements(By.CSS_SELECTOR, 'div.gtAiTL'):
+        banned_times = [e.text for e in driver.find_elements(By.CSS_SELECTOR, 'div.btujzv')]
+    else:
+        banned_times = [e.text for e in driver.find_elements(By.CSS_SELECTOR, 'div.gtAiTL')]
+    
+    driver.close()
+    return {'plate_num': plate_nums_info, 'banned_times': banned_times}
+
+def url_builder(*args) -> str:
+    '''
+    TODO
+    '''
+    base_url, city_path, cat_path, date_path = args
+    base_result = base_url + city_path + cat_path
+    date_path = '/fecha' + date_path if date_path else ''
+    return ( base_result + date_path if date_path else base_result )
+
+def request_pyp_page(pyp_url:str):
+    '''
+    TODO
+    '''
+    try:
+        req = requests.get(pyp_url).content
+    except requests.exceptions.ConnectionError as e:
+        logging.fatal(f'{e}')
+        sys.exit()
+    
+    pyp_page = BeautifulSoup(req, 'html.parser')
+    return pyp_page
+
+def main() -> None:
+    pass    
+    
+
+if __name__ == "__main__":
+    main()
